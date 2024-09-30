e3e232ad00d519cadb3c4fd41bef8c7f69678ddc
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-09-30 20:37:37 +0300

    path fix for github pages

--- src/utils/linkItems.ts
+++ src/utils/linkItems.ts
@@ -1,16 +1,14 @@
-import { base } from '$app/paths';
-
 const linkItems = [
-  { name: 'Αρχική', href: base },
-  { name: 'Πλεονεκτήματα', href: `${base}/why-choose-us` },
+  { name: 'Αρχική', href: '/' },
+  { name: 'Πλεονεκτήματα', href: '/why-choose-us' },
   {
     name: 'FAQ',
     href: '/faq',
   },
 
-  { name: 'Τιμοκατάλογος', href: `${base}/pricing` },
-  { name: 'Πελατολόγιο', href: `${base}/#clients` },
-  { name: 'Επικοινωνία', href: `${base}/contact` },
+  { name: 'Τιμοκατάλογος', href: '/pricing' },
+  { name: 'Πελατολόγιο', href: '/#clients' },
+  { name: 'Επικοινωνία', href: '/contact' },
 ];
 
 export default linkItems;
